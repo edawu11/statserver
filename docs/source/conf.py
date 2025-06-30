5e2d1f03f8d502c8ba09caf1674a75fba3f0f0f7
Yida Wu <edawu1736@gmail.com>
2025-06-30 11:25:00 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- myst-parser 配置 -------------------------------------------------------
-
 myst_enable_extensions = [
     "colon_fence",
     "deflist",
